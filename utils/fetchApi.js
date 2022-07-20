004d99da28daa1fbc74a7cd9c4157ab0e5ad394f
Sunghyun627 <kksscc4@gmail.com>
2022-07-20 17:29:51 +0900

    creat env file for env variables and apply

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-
+import dotenv from 'dotenv';
 export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+dotenv.config();
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-      'X-RapidAPI-Key': 'fe03cf8c63msh86cb93d8a74dbd4p1a392fjsn70ad8ef3d625',
+      'X-RapidAPI-Host': `${process.env.RapidAPIHost}`,
+      'X-RapidAPI-Key': `${process.env.RapidAPIKey}`,
     },
   });
   return data;
