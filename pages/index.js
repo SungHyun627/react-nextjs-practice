fc43ddaf00464b94ae8d53eb917c8b875b4172bd
Sunghyun627 <kksscc4@gmail.com>
2022-07-20 17:00:45 +0900

    remove unnecessary part

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,6 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
   console.log(propertiesForRent, propertiesForSale);
   return (
     <Box>
-      <h1>Hello World</h1>
       <Banner
         purpose="RENT A HOME"
         title1="Rental Homes for"
